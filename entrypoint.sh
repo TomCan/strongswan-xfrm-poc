e8d7864a4442f336d07a496725f451488cb800aa
Tom Cannaerts <tom.cannaerts@intracto.com>
2020-08-13 22:26:59 +0200

    retry until running

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-ipsec start
+# start syslog
+rsyslogd
+
+# run init.sh
 bash /etc/init.sh
 
-while true; do sleep 10000; done
+# start ipsec
+ipsec start
+
+# load config
+swanctl --load-all
+while [ "$R" != "0" ]
+do
+    sleep 0.5
+    swanctl --load-all
+    R=$?
+done
+
+while true; do tail -f /var/log/messages; done
